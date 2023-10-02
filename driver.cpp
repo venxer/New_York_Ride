43fcb3d772d6ee5be5666d88c54951e2f24f3a89
venxer <edwinzhao123@gmail.com>
2023-10-02 02:04:28 -0400

    constuctor + getters

--- driver.cpp
+++ driver.cpp
@@ -15,5 +15,31 @@ Driver::Driver(std::string firstName,
                std::string riderLastName,
                std::string riderPhoneNum)
 {
-
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->gender = gender;
+    this->age = age;
+    this->phoneNum = phoneNum;
+    this->rating = rating;
+    this->latitude = latitude;
+    this->longitude = longitude;
+    this->vehicleType = vehicleType;
+    this->state = state;
+    this->riderFirstName = riderFirstName;
+    this->riderLastName = riderLastName;
+    this->riderPhoneNum = riderPhoneNum;
 }
+
+std::string Driver::getFirstName(){return firstName;}
+std::string Driver::getLastName(){return lastName;}
+std::string Driver::getGender(){return gender;}
+unsigned int Driver::getAge(){return age;}
+std::string Driver::getPhoneNum(){return phoneNum;}
+double Driver::getRating(){return rating;}
+double Driver::getLatitude(){return latitude;}
+double Driver::getLongitude(){return longitude;}
+std::string Driver::getVehicleType(){return vehicleType;}
+std::string Driver::getstate(){return state;}
+std::string Driver::getRiderFirstName(){return riderFirstName;}
+std::string Driver::getRiderLastNam(){return riderLastName;}
+std::string Driver::getRiderPhoneNum(){return riderPhoneNum;}
