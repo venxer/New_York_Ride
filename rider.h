db7c2cf8d97812b126e0261d38aa2415b4b2cd3f
venxer <edwinzhao123@gmail.com>
2023-10-02 02:14:13 -0400

    getters

--- rider.h
+++ rider.h
@@ -23,6 +23,7 @@ class Rider
         std::string driverFirstName;
         std::string driverLastName;
         std::string driverPhoneNum;
+        
     public:
         Rider(std::string firstName,
               std::string lastName,
@@ -41,6 +42,24 @@ class Rider
               std::string driverFirstName,
               std::string driverLastName,
               std::string driverPhoneNum);
+
+        std::string getFirstName(){return firstName;}
+        std::string getLastName(){return lastName;}
+        std::string getGender(){return gender;}
+        unsigned int getAge(){return age;}
+        std::string getPhoneNum(){return phoneNum;}
+        double getRating(){return rating;}
+        std::string getPickupLocation(){return pickupLocation;}
+        double getPickupLatitude(){return pickupLatitude;}
+        double getPickupLongitude(){return pickupLongitude;}
+        std::string getDropoffLocation(){return dropoffLocation;}
+        double getDropoffLatitude(){return dropoffLatitude;}
+        double getDropoffLongitude(){return dropoffLongitude;}
+        std::string getVehiclePref(){return vehiclePref;}
+        std::string getState(){return state;}
+        std::string getDriverFirstName(){return driverFirstName;}
+        std::string getDriverLastName(){return driverLastName;}
+        std::string getDriverPhoneNum(){return driverPhoneNum;}
 };
 
 std::ostream &operator<<(std::ostream &out_str, const Rider &rider);
